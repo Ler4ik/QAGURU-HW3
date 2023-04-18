33b2d20eae9b1e3bcc1abe0319193073f7a5bc9c
sanzharova <valvalsan@gmail.com>
2023-04-18 23:55:50 +0700

    added attaches

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -2,6 +2,7 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -28,5 +29,13 @@ public class TestBase {
     @BeforeEach
     void addListener(){
         SelenideLogger.addListener("AllureSelenide",new AllureSelenide());
+    }
+
+    @AfterEach
+    void addAttachments(){
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        //Attach.addVideo();
     }
 }
