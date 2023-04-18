3d6e8d172eee49b7f6210d0d953787dc2899d62a
sanzharova <valvalsan@gmail.com>
2023-04-19 00:06:33 +0700

    added attach video

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -36,6 +36,6 @@ public class TestBase {
         Attach.screenshotAs("Last screenshot");
         Attach.pageSource();
         Attach.browserConsoleLogs();
-        //Attach.addVideo();
+        Attach.addVideo();
     }
 }
