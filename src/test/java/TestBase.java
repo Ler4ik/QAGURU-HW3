e3da65954ac6d9c4e7c472de458de26a6ad794dd
sanzharova <valvalsan@gmail.com>
2023-04-18 23:21:03 +0700

    added selenoid video

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,5 +1,7 @@
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import java.util.Map;
 
 public class TestBase {
 
@@ -8,5 +10,14 @@ public class TestBase {
         Configuration.baseUrl="https://github.com/";
         Configuration.browserSize = "1920x1080";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("browserName", "chrome");
+        capabilities.setCapability("browserVersion", "100.0");
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
     }
 }
